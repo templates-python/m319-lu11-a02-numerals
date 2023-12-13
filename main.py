8899ffd7f46db501fad26f0333690b1057d9cc6a
Marcel Suter <marcel.suter@bzz.ch>
2023-12-13 07:34:32 +0100

    added template

--- main.py
+++ main.py
@@ -1,12 +1,56 @@
-def calculate():
+def main():
     """
-    calculates the total
+    main program
     :return: None
     """
-    price = 15.75
-    quantity = 3  # TODO change quantity to 5
-    print(f'Total: {price * quantity}')
+    numerals = dict()
+    read_numerals(numerals)
+    letters = input('Schriftzeichen > ')
+    decimal_value = convert(letters, numerals)
+    print(f'{letters} entspricht {decimal_value}')
+
+
+def read_numerals(numeral_dict):
+    """
+    reads the numerals and their values
+    :param numeral_dict:
+    :return: None
+    """
+    # Speichere die Eingabe des Benutzers in Buchstabe
+    # Solange Buchstabe nicht § ist
+        # Speichere den Returnwert von read_int als Zahl
+        # Speichere die Eingaben im Dictionary (Buchstabe ist der Schlüssel, Zahl ist der Wert)
+        # Speichere die Eingabe des Benutzers in Buchstabe
+
+
+def read_int(prompt):
+    """
+    asks the user to input a positive integer
+    :param prompt:
+    :return: number
+    """
+
+    '''
+    Verwende deine Funktion zum Lesen von Ganzzahlen aus einem früheren Programm
+    Ergänze eine Bedingung, dass die Zahl positiv sein muss (grösser 0)
+    '''
+    pass
+
+
+def convert(letters, numeral_dict):
+    """
+    converts the numerals into a decimal number
+    :param letters:
+    :param numeral_dict:
+    :return:
+    """
+    # setze das Total gleich 0
+    # Iteration über alle Buchstaben in letters
+        # nimm den Wert im Dictionary mit dem Buchstaben als Key, addiere diesen Wert zum Total
+
+    # Gib das Total als Returnwert zurück
+    pass
 
 
 if __name__ == '__main__':
-    calculate()
+    main()
--- main_test.py
+++ main_test.py
@@ -1,6 +1,33 @@
 import main
 
-def test(capsys):
-    main.calculate()
+
+def test_roman(capsys, monkeypatch):
+    inputs = iter(['I', '1', 'V', '5', 'X', '10', 'C', '100', '§', 'CXXVII'])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main.main()
     captured = capsys.readouterr()
-    assert captured.out == "Total: 78.75\n"
+    assert captured.out == "CXXVII entspricht 127\n"
+
+
+def test_assyrian(capsys, monkeypatch):
+    inputs = iter(['L', '1', 'C', '12', 'V', '144', 'X', '288', '§', 'XXCLLLLL'])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main.main()
+    captured = capsys.readouterr()
+    assert captured.out == "XXCLLLLL entspricht 593\n"
+
+
+def test_not_a_number(capsys, monkeypatch):
+    inputs = iter(['A', 'B', '1', '§', 'A'])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main.main()
+    captured = capsys.readouterr()
+    assert captured.out == "Gib eine positive Ganzzahl ein\nA entspricht 1\n"
+
+
+def test_negative_number(capsys, monkeypatch):
+    inputs = iter(['A', '-3', '1', '§', 'A'])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main.main()
+    captured = capsys.readouterr()
+    assert captured.out == "Gib eine positive Ganzzahl ein\nA entspricht 1\n"
