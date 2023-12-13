b8191b6e9b1ae1c345017fb15caabcd08a1b05b2
Marcel Suter <marcel.suter@bzz.ch>
2023-12-13 07:26:10 +0100

    added solution

--- main.py
+++ main.py
@@ -1,12 +1,59 @@
-def calculate():
+def main():
     """
-    calculates the total
+    main program
     :return: None
     """
-    price = 15.75
-    quantity = 3  # TODO change quantity to 5
-    print(f'Total: {price * quantity}')
+    numerals = dict()
+    read_numerals(numerals)
+    letters = input('Schriftzeichen > ')
+    decimal_value = convert(letters, numerals)
+    print(f'{letters} entspricht {decimal_value}')
+
+
+def read_numerals(numeral_dict):
+    """
+    reads the numerals and their values
+    :param numeral_dict:
+    :return: None
+    """
+    character = input('Zahlzeichen (§=Ende) > ')
+    while character != '§':
+        value = read_int('Wert')
+        numeral_dict[character] = value
+        character = input('Zahlzeichen (§=Ende) > ')
+
+
+def read_int(prompt):
+    """
+    asks the user to input a positive integer
+    :param prompt:
+    :return: number
+    """
+    number = None
+    while number is None:
+        try:
+            number = int(input(f'{prompt} > '))
+            if number < 0:
+                print('Gib eine positive Ganzzahl ein')
+                number = None
+        except ValueError:
+            print('Gib eine positive Ganzzahl ein')
+    return number
+
+
+def convert(letters, numeral_dict):
+    """
+    converts the numerals into a decimal number
+    :param letters:
+    :param numeral_dict:
+    :return:
+    """
+    total = 0
+    for character in letters:
+        total += numeral_dict[character]
+
+    return total
 
 
 if __name__ == '__main__':
-    calculate()
+    main()
